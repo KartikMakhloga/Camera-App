61ee99f4603b2acc6f3a3192ed6b8a63d79e781d
kartik <kartikmakhloga20@gmail.com>
2022-12-11 18:13:24 +0530

    added gallery page

--- dbScript.js
+++ dbScript.js
@@ -1,4 +1,5 @@
 let req = indexedDB.open("Camera", 1);
+let div = document.querySelector(".container");
 let db;
 
 req.addEventListener("success", function () {
@@ -20,7 +21,6 @@ function addMedia(media, type) {
   gallery.add(obj);
 }
 
-
 function viewMedia() {
   if (!db) return;
 
@@ -30,11 +30,35 @@ function viewMedia() {
 
   cursorReq.addEventListener("success", function () {
     let cursor = cursorReq.result;
+    let MediaContainer = document.createElement("div");
+    MediaContainer.classList.add("media-container");
+    let LinkForDownload = "";
     if (cursor) {
-      console.log(cursor.value);
-      cursor.continue;
+      let mo = cursor.value;
+      if (mo.type == "video") {
+        //I have to render a video tag
+        let url = window.URL.createObjectURL(cursor.value.media);
+        LinkForDownload = url;
+        MediaContainer.innerHTML = `<div class="media">
+                                    <video src="${url}" autoplay loop controls muted></video>
+                                     </div>
+                                     <button class="download">Download</button>
+                                     <button class="delete">Delete</button>`;
+      } else {
+        //I have to render a image tag
+        LinkForDownload = cursor.value.media;
+        MediaContainer.innerHTML = `<div class="media">
+                                   <img src="${cursor.value.media}" />
+                                   </div>
+                                   <button class="download">Download</button>
+                                   <button class="delete">Delete</button>`;
+      }
+      
+      
+
+      div.appendChild(MediaContainer);
+      console.log(cursor);
+      cursor.continue();
     }
   });
 }
-
-
